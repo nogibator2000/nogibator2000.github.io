978d5066b8ac4d87a083a00082a9ec98c00c7d4d
nogibator2000 <nogibator2000@gmail.com>
2020-01-15 23:43:34 +0300

    Updates

---
+++ precache-manifest.a114594c40fb7f5a956d81a7e98a86a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd505bccedd62fc72c989946d99664a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cbcb90793975ae569b9",
+    "url": "/static/css/2.06d4c009.chunk.css"
+  },
+  {
+    "revision": "52b9bd29beb805cde642",
+    "url": "/static/css/main.08772044.chunk.css"
+  },
+  {
+    "revision": "9cbcb90793975ae569b9",
+    "url": "/static/js/2.066d91c6.chunk.js"
+  },
+  {
+    "revision": "21d6373660cb6235c0c27fda28b1e7b3",
+    "url": "/static/js/2.066d91c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "52b9bd29beb805cde642",
+    "url": "/static/js/main.e47564c2.chunk.js"
+  },
+  {
+    "revision": "0446baeef641abda518f",
+    "url": "/static/js/runtime-main.8b2f120f.js"
+  }
+]);
