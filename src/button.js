1b6e8c37118dbb2c0dd7a6ce31f215af6931adfd
nogibator2000 <nogibator2000@gmail.com>
2020-01-16 20:11:31 +0300

    v0.0.2

--- src/button.js
+++ src/button.js
@@ -12,7 +12,7 @@ this.setState({tColor: theme.tc2, color: this.props.colorH == null?theme.altColo
 mouseLeave = () => {
 this.setState({tColor: theme.tc, color: this.props.color == null?theme.menuBGIA:this.props.color})}
   render() {
-      return(<div onClick={this.props.click} onMouseEnter={this.mouseEnter} onMouseLeave={this.mouseLeave} className="Button"
+      return(<div onClick={this.props.click} onMouseIn ={this.mouseEnter} onMouseMove={this.mouseEnter} onMouseLeave={this.mouseLeave} className="Button"
       style={{cursor:'pointer', borderRight: theme.border, background: this.state.color, display: 'flex',
        flexDirection: 'row', margin: 'auto',alignItems: 'center' , width: this.props.width, height: this.props.height}}>
         <h4 style={{width: '100%',textAlign: 'center', color: this.state.tColor}}>{this.props.name}</h4>
--- src/chess.js
+++ src/chess.js
@@ -5,7 +5,6 @@ import{Link} from "react-router-dom";
 import {theme} from './theme';
 import "../node_modules/video-react/dist/video-react.css";
 
-
 export default class Chess extends Component{
   constructor(props) {
    super(props);
@@ -18,6 +17,80 @@ export default class Chess extends Component{
    this.setState({showWiki: !this.state.showWiki});
  }
   render() {
+    const howBtn = <div><Button width = {window.innerWidth/4} key={this.state.showHow} height = {window.innerWidth/16}  color = {this.state.showHow?theme.menuBGIA:theme.BTMain} colorH = {this.state.showHow?theme.altColor:theme.BTMainA} name='How to run' click={this.toggleHow}/>
+    </div>;
+    const wikiBtn = <div><Button width = {window.innerWidth/4} key ={this.state.showWiki} height = {window.innerWidth/16}  color = {this.state.showWiki?theme.menuBGIA:theme.BTMain} colorH = {this.state.showWiki?theme.altColor:theme.BTMainA} name='Wiki' click={this.toggleWiki}/>
+    </div>;
+    const wiki = <div><br /> <h4>Wiki</h4>
+  <br/>
+  Open setting file. Inside bin derictory. If there is none run app, it will be autocreated.
+  <br/>
+    <pre>
+      BoardSize.Width<br/>
+      BoardSize.Height<br/>
+      BoardPosition.X<br/>
+      BoardPosition.Y<br/>
+    </pre>
+    Parameters automaticly setting up by calibrate button. <br/>
+  <pre>
+  Hash<br/>
+  RefreshDelay<br/>
+  AnimationDelay<br/>
+  AnimationHash<br/>
+  </pre>
+  Parameter of hash function lower it(or increase delay) to increase speed and increase miss turn chance
+  <pre>
+    MaxEngineTime<br/>
+  </pre>
+  Engine time per turn in ms.
+  <pre>
+    MissplayEveryXTurns
+  </pre>
+  You need those missplays to avoid ban, set 0 to fully play like chess Engine.
+  <br/>
+  <pre>
+    Window<br/>
+    Treshold<br/>
+  </pre>
+  Thouse parameters are avoid your calibration inaccuracy.
+  <pre>
+    WhiteBrightTreshold<br/>
+    BlackBrightTreshold<br/>
+  </pre>
+   Parameters for classify backgroud black and white pieses.
+  <pre>
+    TurnDelayMin<br/>
+    TurnDelayMax<br/>
+    TurnsForExtraDelay<br/>
+    DelayPart<br/>
+    MouseClickDelay<br/>
+  </pre>
+  Customise your turn to make in more humanable.
+  <pre>
+  StockfishPath
+  </pre>
+  Insert here your stockFish or any other uci engine path.
+  <pre>
+  LogEnable
+  </pre> Debug option, when enable logging data.            </div>;
+
+  const how = <div>
+      <br /><h4> How to run</h4> <br/> &#183; Clone the <a href="https://github.com/nogibator2000/ChessBot">project</a>
+     or skip it and <Link to="../file/ChessBot.zip" target="_blank" download> download</Link> binaries
+    <br/>
+    <br/>
+    <pre> git clone https://github.com/nogibator2000/ChessBot</pre>
+    <br/>
+    &#183; open it with Microsoft Visual Studio.
+    <br/>
+    &#183; Compile it and run.
+    <br/>
+    &#183; Open a chess website.
+    <br/>
+    &#183; Click Colibrate button then click top left point of chess board, then click bottom right point of chess board.
+    <br/>
+    &#183; Your bot is ready to run. Press F2 or start button to start.
+    </div>;
     return (
       <div className='page' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', color: theme.tc, background: theme.pc}}>
       <h2>Screen detection chess bot</h2>
@@ -27,7 +100,7 @@ export default class Chess extends Component{
         width={window.innerWidth/2}
         height={window.innerWidth/4}
         autoPlay={true}
-        poster="../file/loading.mp4"
+        poster="../file/poster.jpg"
         muted={true}
         src="../file/loading.mp4"
       />
@@ -36,81 +109,14 @@ export default class Chess extends Component{
        Download it, open any chess site, show to it your board and press start button.
         It will work but only if you have windows. This bot was tested on those sites <a href='https:Lichess.org'>Lichess.org</a> and <a href='https:Chess.com'>Chess.com</a> WITH NO BAN.</h5>
     <Link to="../file/ChessBot.zip" target="_blank" download>
-      <Button width = {window.innerWidth/4} height = {window.innerWidth/16}  color = {'#6060ff'} colorH = {'#2020cF'} name='Download .zip' />
+      <Button width = {window.innerWidth/4} height = {window.innerWidth/16}  color = {theme.BTMain} colorH = {theme.BTMainA} name='Download .zip' />
   </Link>
       <br />
-    <Button width = {window.innerWidth/4} height = {window.innerWidth/16}   color = {'#6060ff'} colorH = {'#2020cF'} name='How to run' click={this.toggleHow}/>
-  {this.state.showHow&&<div><br /><h4> How to run</h4> <br/> &#183; Clone the <a href="https://github.com/nogibator2000/ChessBot">project</a>
-   or skip it and <Link to="../file/ChessBot.zip" target="_blank" download> download</Link> binaries
-  <br/>
-<br/>
-<pre> git clone https://github.com/nogibator2000/ChessBot</pre>
-<br/>
-&#183; open it with Microsoft Visual Studio.
-<br/>
-&#183; Compile it and run.
-<br/>
-&#183; Open a chess website.
-<br/>
-&#183; Click Colibrate button then click top left point of chess board, then click bottom right point of chess board.
-<br/>
-&#183; Your bot is ready to run. Press F2 or start button to start.
-</div>}
+      {howBtn}
+  {this.state.showHow&&how}
       <br />
-    <Button width = {window.innerWidth/4} height = {window.innerWidth/16}  color = {'#6060ff'} colorH = {'#2020cF'} name='Wiki' click={this.toggleWiki}/>
-  {this.state.showWiki&&<div><br /> <h4>Wiki</h4>
-<br/>
-Open setting file. Inside bin derictory. If there is none run app, it will be autocreated.
-<br/>
-  <pre>
-    BoardSize.Width<br/>
-    BoardSize.Height<br/>
-    BoardPosition.X<br/>
-    BoardPosition.Y<br/>
-  </pre>
-  Parameters automaticly setting up by calibrate button. <br/>
-<pre>
-Hash<br/>
-RefreshDelay<br/>
-AnimationDelay<br/>
-AnimationHash<br/>
-</pre>
-Parameter of hash function lower it(or increase delay) to increase speed and increase miss turn chance
-<pre>
-  MaxEngineTime<br/>
-</pre>
-Engine time per turn in ms.
-<pre>
-  MissplayEveryXTurns
-</pre>
-You need those missplays to avoid ban, set 0 to fully play like chess Engine.
-<br/>
-<pre>
-  Window<br/>
-  Treshold<br/>
-</pre>
-Thouse parameters are avoid your calibration inaccuracy.
-<pre>
-  WhiteBrightTreshold<br/>
-  BlackBrightTreshold<br/>
-</pre>
- Parameters for classify backgroud black and white pieses.
-<pre>
-  TurnDelayMin<br/>
-  TurnDelayMax<br/>
-  TurnsForExtraDelay<br/>
-  DelayPart<br/>
-  MouseClickDelay<br/>
-</pre>
-Customise your turn to make in more humanable.
-<pre>
-StockfishPath
-</pre>
-Insert here your stockFish or any other uci engine path.
-<pre>
-LogEnable
-</pre>
-Debug option, when enable logging data.            </div>}
+      {wikiBtn}
+  {this.state.showWiki&&wiki}
       <br />
 </div>
     );
